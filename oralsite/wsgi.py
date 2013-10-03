a8e275aa4b8d7fe3614e23424a7738c2c3120809
Alexandre Leray <alexandre@alexandreleray.com>
2013-10-04 00:49:11 +0200

    Multiple sites running Django on Apache mod WSGI does not work with the default wsgy.py file, so this is the fix.
    See: http://stackoverflow.com/questions/12134441/multiple-sites-using-django-and-mod-wsgi-on-apache

--- oralsite/wsgi.py
+++ oralsite/wsgi.py
@@ -15,7 +15,7 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oralsite.settings")
+os.environ["DJANGO_SETTINGS_MODULE"] = "oralsite.settings"
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
