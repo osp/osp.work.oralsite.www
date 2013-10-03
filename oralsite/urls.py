8af78c19b365e2f94a93eddd580065170b0facdf
Eric Schrijver <eric@ericschrijver.nl>
2013-10-03 13:17:56 +0200

    Set up automatic redirects for / to /pages/ and from /pages/ to /pages/Index/

--- oralsite/urls.py
+++ oralsite/urls.py
@@ -3,12 +3,14 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 
+from django.views.generic import RedirectView
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^pages/', include('aawiki.urls')),
+    url(r'^$', RedirectView.as_view(url='/pages/')),
 )
 
 
