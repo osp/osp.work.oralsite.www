a5579199edce253747ecd151f84bf9bbe9730baf
Eric Schrijver <eric@ericschrijver.nl>
2014-01-21 14:49:41 +0100

    Change filter saving strategy: create an arborescence that maps to the url
    Create a processed view, that serves static files, and if not found,
    redirects to the process view.
    
    Make the process view synchronous.
    (Make it asynchronous again by passing a GET parameter async).
    
    Make the process return a redirect to the resource.
    (To make it return the info view, pass a GET parameter info)
    
    Adapted a bit the front-end code, add an example of the pipeline
    to the tests.

--- aafilters/views.py
+++ aafilters/views.py
@@ -4,14 +4,36 @@ from __future__ import absolute_import
 # becomes `proj.celery.schedules` in Python 2.x since it allows
 # for relative imports by default.
 
+import os
 
 from django.shortcuts import redirect
 from .tasks import process_pipeline
+from djcelery.views import task_status
 
+def process(request, pipeline_string):
+    """
+    With a url like /filters/process/http://s2.lemde.fr/image/2012/05/09/644x322/1698586_3_83ef_francois-hollande-et-nicolas-sarkozy-durant-la_cc28a6e60a381054c901fecf8fe39886.jpg..bw.jpg
+    
+    Find:
+    url = 'http://s2.lemde.fr/image/2012/05/09/644x322/1698586_3_83ef_francois-hollande-et-nicolas-sarkozy-durant-la_cc28a6e60a381054c901fecf8fe39886.jpg'
+    extension = '.jpg'
+    pipeline = '[u'bw']'
+    
+    And send it of to Celery
+    """
+    parts = pipeline_string.split('..')
+    url = parts[0]
+    pipeline = []
+    if parts > 1:
+        pipeline = parts[1:]
+        pipeline[-1], extension = os.path.splitext(pipeline[-1])
+    print url, pipeline_string, extension
+    if 'async' in request.GET:
+        task_id = process_pipeline(url=url, pipeline=pipeline, target_ext=extension)
+        return task_status(request, task_id=task_id)
+    else:
+        task_id = process_pipeline(url=url, pipeline=pipeline, target_ext=extension, synchronous=True)
+    if 'info' in request.GET:
+        return task_status(request, task_id=task_id)
+    return redirect('processed', path=pipeline_string)
 
-def process(request):
-    url = request.GET.get('url')
-    pipeline = request.GET.get('pipeline')
-    pipeline = pipeline.split('|')
-    task_id = process_pipeline(url=url, pipeline=pipeline)
-    return redirect('celery-task_status', task_id=task_id)
