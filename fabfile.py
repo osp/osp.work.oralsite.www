7020ed461d5199f75fea1bda5dbfc0d61dc4bc14
Alexandre Leray <alexandre@stdin.fr>
2014-01-29 00:49:06 +0100

    Some care to deploy Olga

---
+++ fabfile.py
@@ -0,0 +1,21 @@
+import os.path
+from fabric.api import run, local, put, cd, sudo, env, prefix
+from fabric.contrib.console import confirm
+
+
+env.hosts = ['sarma@sarma.be']
+env.path = '/srv/sarma_data01/www/apps/be.oralsite'
+
+
+def deploy():
+    """deploys to previously setup environment"""
+    path_activate = '/srv/sarma_data01/www/venvs/be.oralsite/bin/activate'
+    path_wsgi = '/srv/sarma_data01/www/apps/be.oralsite/oralsite/wsgi.py'
+
+    with cd(env.path):
+        run('git pull origin master')
+
+        with prefix('source %s' % path_activate):
+            run('python manage.py collectstatic --noinput')
+
+    run('touch %s' % path_wsgi)
