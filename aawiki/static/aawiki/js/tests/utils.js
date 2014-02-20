7c0ce86b10187c73b45b39e65cd2536c23907412
Eric Schrijver <eric@ericschrijver.nl>
2014-02-20 14:57:25 +0100

    Make test for reading mime-type from uri with fragment identifier

--- aawiki/static/aawiki/js/tests/utils.js
+++ aawiki/static/aawiki/js/tests/utils.js
@@ -20,14 +20,20 @@ describe("The AA utils", function() {
         it("knows to find a mime-type for a filename", function(){
             expect(AA.utils.path2mime("poster.jpg")).toBe("image/jpeg");
         });
-        it("this also works for a url", function(){
-            expect(AA.utils.path2mime("http://example.com/poster.jpg")).toBe("image/jpeg");
+    });
+    
+    describe("The utility uri2mime", function(){
+        it("is like path2mime, yet it works for a url", function(){
+            expect(AA.utils.uri2mime("http://example.com/poster.jpg")).toBe("image/jpeg");
+        });
+        it("even when using fragment identifiers", function(){
+            expect(AA.utils.uri2mime("http://example.com/poster.jpg#xywh=160,120,320,240")).toBe("image/jpeg");
         });
         it("if there is no extension, it as served as `application/octet-stream`", function(){
-            expect(AA.utils.path2mime("http://example.com/poster")).toBe("application/octet-stream");
+            expect(AA.utils.uri2mime("http://example.com/poster")).toBe("application/octet-stream");
         });
         it("this is also the case if the extension is not recognised", function(){
-            expect(AA.utils.path2mime("http://example.com/poster.some-proprietary-extension")).toBe("application/octet-stream");
+            expect(AA.utils.uri2mime("http://example.com/poster.some-proprietary-extension")).toBe("application/octet-stream");
         });
     });
   
